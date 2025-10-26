122ee06a28a9b3e3d2f7875ecd7f12aded032b19
FranciscaGuzmanPerez <francisca.guzman.p.03@gmail.com>
2025-10-25 21:41:39 -0300

    Add Task 1 and implement basic Add for two comma-separated numbers

---
+++ Task1/Task1.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let Add (numbers: string) =
+    let parts = numbers.Split(',')
+    if parts.Length = 2 then
+        let a = int parts[0]
+        let b = int parts[1]
+        a + b
+    else
+        0
+
+[<EntryPoint>]
+let main argv =
+    printfn "Result of '1,2' -> %d" (Add "1,2")
+    0
