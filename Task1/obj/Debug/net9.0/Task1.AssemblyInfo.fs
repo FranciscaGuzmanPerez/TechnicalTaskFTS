112975b2f1276ae5d19a2fadac9f0dbc67206168
FranciscaGuzmanPerez <francisca.guzman.p.03@gmail.com>
2025-10-26 15:18:05 -0300

    Add Task4

--- Task1/obj/Debug/net9.0/Task1.AssemblyInfo.fs
+++ Task1/obj/Debug/net9.0/Task1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Task1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efcbbad4fe076c7b9318045810f76d0ac57a789e")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5f620ba805ffae083ce1a4a676b5830be12032")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Task1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Task1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
